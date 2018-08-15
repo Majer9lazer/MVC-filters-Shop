5d0d9aa82b78364803b4b04b32035c6373249fa5
СидоренкоЕ <@401_7>
2018-08-15 17:56:51 +0300

    Добавьте файлы проекта.

---
+++ MVC-Filters/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ActionFilter]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ActionFilter]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [ActionFilter]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
